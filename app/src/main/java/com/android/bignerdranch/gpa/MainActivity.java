b516d28759718aa3ba12b913308c2719eee35e3c
Davidson-09 <davidsonakra@gmail.com>
2021-04-28 13:12:48 +0100

    added course item, included viewpager2 along with the adapter

--- app/src/main/java/com/android/bignerdranch/gpa/MainActivity.java
+++ app/src/main/java/com/android/bignerdranch/gpa/MainActivity.java
@@ -1,14 +1,25 @@
 package com.android.bignerdranch.gpa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    ViewPager2 vp;
+    ViewPagerAdapter adapter;
+
+    @Override 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vp = findViewById(R.id.viewPager2);
+        adapter = new ViewPagerAdapter();
+        vp.setAdapter(adapter);
     }
 }
